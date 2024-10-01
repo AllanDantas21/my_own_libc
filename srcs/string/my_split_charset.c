ff3a2f7993e93bc813ac696c48af790e09e7d6a1
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-30 21:43:17 -0300

    add split and strjoin ultimate

---
+++ srcs/string/my_split_charset.c
@@ -0,0 +1,90 @@
+#include "my_libc.h"
+
+static char	*my_substr(char *str, unsigned int start, int len)
+{
+	char	*substr;
+	int		i;
+
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = str[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+static int	has_on_charset(char c, char *charset)
+{
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+static int	count_words(char *str, char *charset)
+{
+	int	words;
+
+	if (!str)
+		return (0);
+	words = 0;
+	while (*str)
+	{
+		while (*str && has_on_charset(*str, charset))
+			str++;
+		if (*str && !has_on_charset(*str, charset))
+			words++;
+		while (*str && !has_on_charset(*str, charset))
+			str++;
+	}
+	return (words);
+}
+
+static void	fill_matrix(char *str, char *charset, char **arr)
+{
+	int		letters;
+	int		pos;
+
+	pos = 0;
+	if (str)
+	{
+		letters = 0;
+		while (*str)
+		{
+			while (*str && has_on_charset(*str, charset))
+				str++;
+			while (*str && !has_on_charset(*str, charset))
+			{
+				letters++;
+				str++;
+			}
+			arr[pos] = my_substr(str - letters, 0, letters);
+			pos++;
+			letters = 0;
+		}
+	}
+}
+
+char	**my_split(char *str, char *charset)
+{
+	char	**arr;
+	size_t	words;
+
+	if (!str)
+		return (NULL);
+	words = count_words(str, charset);
+	arr = malloc(sizeof(char *) * (words + 1));
+	if (!arr)
+		return (NULL);
+	fill_matrix(str, charset, arr);
+	arr[words] = 0;
+	return (arr);
+}
---
+++ srcs/string/my_strjoin_matrix.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+static int	my_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static void	fill_string(char *str, int size, char **strs, char *sep)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	while (i < size)
+	{
+		k = 0;
+		while (strs[i][k])
+			*str++ = strs[i][k++];
+		k = 0;
+		while (i < size - 1 && sep[k])
+			*str++ = sep[k++];
+		i++;
+	}
+	*str = 0;
+}
+
+static int	get_bytes(int i, int size, char **strs)
+{
+	int	bytes;
+
+	bytes = 0;
+	while (i < size)
+		bytes += my_strlen(strs[i++]);
+	return (bytes);
+}
+
+char	*my_strjoin(int size, char **strs, char *sep)
+{
+	char	*str;
+	int		bytes;
+	int		i;
+
+	if (size == 0)
+		return (malloc(1));
+	i = 0;
+	bytes = get_bytes(i, size, strs);
+	str = malloc(bytes + (size - 1) * my_strlen(sep) + 1);
+	if (!str)
+		return (NULL);
+	fill_string(str, size, strs, sep);
+	return (str);
+}
