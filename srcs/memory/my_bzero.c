dec3b7353120d533450b36b559e5c94527bc1670
Allandantas21 <Allandantas21.ad@gmail.com>
2024-10-17 23:18:30 -0300

    add doc in bzero

--- srcs/memory/my_bzero.c
+++ srcs/memory/my_bzero.c
@@ -1,5 +1,17 @@
 #include "my_libc.h"
 
+/**
+ * @brief Sets the first n bytes of the block of memory pointed by s to zero.
+ *
+ * @param s Pointer to the block of memory to fill.
+ * 		This block of memory is assumed to be at least n bytes long.
+ * @param n Number of bytes to be set to zero.
+ *		size_t is an unsigned integer type.
+ *		Therefore, the value of n cannot be negative.
+ *	Its maximum value is SIZE_MAX.
+ * @return void* A pointer to the memory block s.
+ */
+
 void	*my_bzero(void *s, size_t n)
 {
 	uint64_t	*long_ptr;
