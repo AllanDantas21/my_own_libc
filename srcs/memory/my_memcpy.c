c1cd68a73b1d6eb7bdbc9c4daefad65cd79240fa
Allandantas21 <Allandantas21.ad@gmail.com>
2024-10-18 00:04:14 -0300

    improve memcpy

--- srcs/memory/my_memcpy.c
+++ srcs/memory/my_memcpy.c
@@ -1,15 +1,44 @@
 #include "my_libc.h"
 
+/**
+ * @file my_memcpy.c
+ * @brief Implementation of the my_memcpy function.
+ *
+ * This file contains the implementation of the my_memcpy function, which
+ * copies n bytes from memory area src to memory area dest.
+ */
+
+void	copy_chunks(unsigned long *d_long, const unsigned long *s_long, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n / sizeof(unsigned long); i++)
+		d_long[i] = s_long[i];
+}
+
+void	copy_remaining_bytes(char *d_char, const char *s_char, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n % sizeof(unsigned long); i++)
+		d_char[i] = s_char[i];
+}
+
 void	*my_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*d;
-	char	*s;
+	unsigned long		*d_long;
+	const unsigned long	*s_long;
+	const char			*s_char;
+	char				*d_char;
 
 	if (!src && !dest)
 		return (dest);
-	d = (char *)dest;
-	s = (char *)src;
-	while (n--)
-		*d++ = *s++;
+	d_long = (unsigned long *)dest;
+	s_long = (const unsigned long *)src;
+	copy_chunks(d_long, s_long, n);
+	d_char = (char *)(d_long + (n / sizeof(unsigned long)));
+	s_char = (const char *)(s_long + (n / sizeof(unsigned long)));
+	copy_remaining_bytes(d_char, s_char, n);
+
 	return (dest);
 }
--- tests.c
+++ tests.c
@@ -2,35 +2,41 @@
 # include <stdio.h>
 # include <string.h>
 
-void test_my_memcmp() {
-    char str1[] = "Hello, World!";
-    char str2[] = "Hello, World!";
-    char str3[] = "Hello, world!";
-    char str4[] = "Hello";
+void test_my_memcpy() {
+    char src1[] = "Hello, World!";
+    char dest1[50];
+    char src2[] = "Test";
+    char dest2[50];
+    char src3[] = "Another test";
+    char dest3[50];
 
-    // Test 1: Identical strings
-    if (my_memcmp(str1, str2, strlen(str1)) == 0) {
+    // Test 1: Basic copy
+    my_memcpy(dest1, src1, strlen(src1) + 1);
+    if (strcmp(dest1, src1) == 0) {
         printf("Test 1 passed\n");
     } else {
         printf("Test 1 failed\n");
     }
 
-    // Test 2: Different strings (case-sensitive)
-    if (my_memcmp(str1, str3, strlen(str1)) != 0) {
+    // Test 2: Copy with different length
+    my_memcpy(dest2, src2, strlen(src2) + 1);
+    if (strcmp(dest2, src2) == 0) {
         printf("Test 2 passed\n");
     } else {
         printf("Test 2 failed\n");
     }
 
-    // Test 3: Different lengths
-    if (my_memcmp(str1, str4, strlen(str4)) != 0) {
+    // Test 3: Copy into an empty destination
+    my_memcpy(dest3, src3, strlen(src3) + 1);
+    if (strcmp(dest3, src3) == 0) {
         printf("Test 3 passed\n");
     } else {
         printf("Test 3 failed\n");
     }
 
-    // Test 4: Zero length comparison
-    if (my_memcmp(str1, str2, 0) == 0) {
+    // Test 4: Zero length copy
+    my_memcpy(dest1, src1, 0);
+    if (strcmp(dest1, src1) == 0) {
         printf("Test 4 passed\n");
     } else {
         printf("Test 4 failed\n");
@@ -38,6 +44,6 @@ void test_my_memcmp() {
 }
 
 int main() {
-    test_my_memcmp();
+    test_my_memcpy();
     return 0;
 }
