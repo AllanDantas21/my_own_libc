aa2578f2c86fdca29b377517d1f291e59313b28d
Allandantas21 <Allandantas21.ad@gmail.com>
2024-10-21 20:12:26 -0300

    add btree folder and create node func

---
+++ srcs/btree/btree_create_node.c
@@ -0,0 +1,13 @@
+
+t_btree *btree_create_node(void *item)
+{
+    t_btree *node;
+
+    node = malloc(sizeof(t_btree));
+    if (node == NULL)
+        return (NULL);
+    node->item = item;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
