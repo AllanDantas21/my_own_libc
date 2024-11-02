9821ee5ff3021fbda19c4dca8294d7e9193ba0e1
Allandantas21 <Allandantas21.ad@gmail.com>
2024-11-02 17:59:56 -0300

    add printf prototypes

--- incs/prototypes.h
+++ incs/prototypes.h
@@ -52,4 +52,15 @@ void	my_lstdelone(t_list *lst, void (*del)(void *));
 void	my_lstclear(t_list **lst, void (*del)(void *));
 void	my_lstadd_front(t_list **lst, t_list *new);
 
+// printf functions
+int     my_print_address(void *pointer);
+int     my_print_unsigned(unsigned int n);
+int     my_print_string(const char *str);
+int     my_print_hexa(unsigned int n);
+int     my_print_hexa_upper(unsigned int n);
+int     my_print_decimal(int decimal);
+int     my_print_char(char c);
+char	*utoa_base(unsigned long long n, int base, char *chr);
+
+
 #endif
