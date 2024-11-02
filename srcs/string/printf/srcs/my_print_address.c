b2351e54735ebebcef5f58469217bbd5492b38b5
Allandantas21 <Allandantas21.ad@gmail.com>
2024-11-02 17:57:34 -0300

    finish printf files

---
+++ srcs/string/printf/srcs/my_print_address.c
@@ -0,0 +1,21 @@
+#include "my_libc.h"
+
+int	my_print_address(void *pointer)
+{
+	int					counter;
+	char				*str;
+	unsigned long long	address;
+
+	counter = 0;
+	address = (unsigned long long)pointer;
+	if (!pointer)
+	{
+		counter = my_print_string("(nil)");
+		return (counter);
+	}
+	str = utoa_base(address, 16, "0123456789abcdef");
+	counter += my_print_string("0x");
+	counter += my_print_string(str);
+	free(str);
+	return (counter);
+}
