cf1deb7ccfedc3e93edcff381f85404e7978890f
Allandantas21 <Allandantas21.ad@gmail.com>
2024-11-03 11:58:13 -0300

    add doc in memmove

--- srcs/memory/my_memmove.c
+++ srcs/memory/my_memmove.c
@@ -1,5 +1,20 @@
 #include "my_libc.h"
 
+/**
+ * @file my_memmove.c
+ * @brief Custom implementation of the memmove function.
+ *
+ * This function copies `n` bytes from memory area `src` to memory area `dest`.
+ * The memory areas may overlap: copying takes place as though the bytes in `src`
+ * are first copied into a temporary array that does not overlap `src` or `dest`,
+ * and the bytes are then copied from the temporary array to `dest`.
+ *
+ * @param dest Pointer to the destination memory area.
+ * @param src Pointer to the source memory area.
+ * @param n Number of bytes to copy.
+ * @return Pointer to the destination memory area (`dest`).
+ */
+
 void	*my_memmove(void *dest, const void *src, size_t n)
 {
 	char	*d;
