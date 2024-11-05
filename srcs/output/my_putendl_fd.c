e5420c4fa04f828aaaaa49781cc0b8179242a5dc
Allandantas21 <Allandantas21.ad@gmail.com>
2024-11-05 11:39:54 -0300

    change putendl

--- srcs/output/my_putendl_fd.c
+++ srcs/output/my_putendl_fd.c
@@ -1,5 +1,15 @@
 #include "my_libc.h"
 
+/**
+ * my_putendl_fd - Outputs the string 's' to the given file descriptor
+ * followed by a newline.
+ * @s: The string to output.
+ * @fd: The file descriptor on which to write.
+ *
+ * This function writes the string 's' to the file descriptor specified by 'fd',
+ * followed by a newline character. If 's' is NULL, the function does nothing.
+ */
+ 
 void	my_putendl_fd(char *s, int fd)
 {
 	if (!s)
