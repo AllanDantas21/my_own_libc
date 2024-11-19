9159dd89110910a632f53c00c1b3c69778575dcb
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-19 20:21:26 -0300

    add docstrings

--- srcs/string/str/my_str_tolower.c
+++ srcs/string/str/my_str_tolower.c
@@ -1,5 +1,17 @@
 #include "my_libc.h"
 
+/**
+ * @file my_str_tolower.c
+ * @brief Converts all characters in a string to lowercase.
+ *
+ * This function takes a string and converts all of its characters to lowercase
+ * using the my_tolower function. If the input string is NULL, the function 
+ * returns immediately without performing any operations.
+ *
+ * @param s The string to be converted to lowercase. If s is NULL, the function 
+ *          does nothing.
+ */
+ 
 void    my_str_tolower(char *s)
 {
     if (s == NULL)
--- srcs/string/str/my_str_toupper.c
+++ srcs/string/str/my_str_toupper.c
@@ -1,5 +1,16 @@
 #include "my_libc.h"
 
+/**
+ * @file my_str_toupper.c
+ * @brief Converts all lowercase letters in a string to uppercase.
+ *
+ * This function takes a string and converts all of its lowercase letters
+ * to uppercase using the my_toupper function. If the input string is NULL,
+ * the function returns immediately without making any changes.
+ *
+ * @param s A pointer to the string to be converted to uppercase.
+ */
+ 
 void    my_str_toupper(char *s)
 {
     if (s == NULL)
