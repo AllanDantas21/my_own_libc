599c90021e28eecc8c2b0e95397e07b6d2e4f44e
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-18 21:28:14 -0300

    change substr

--- srcs/string/my_substr.c
+++ srcs/string/my_substr.c
@@ -1,23 +1,19 @@
 #include "my_libc.h"
-
 char	*my_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	s_len;
-	unsigned int	max_len;
-	char			*sub;
+	char	*sub;
+	size_t	s_len;
 
 	if (!s)
 		return (NULL);
 	s_len = my_strlen(s);
-	if (start < s_len)
-		max_len = s_len - start;
 	if (start >= s_len)
-		max_len = 0;
-	if (max_len > len)
-		max_len = len;
-	sub = (char *)malloc((max_len + 1) * sizeof(char));
-	if (sub == NULL)
+		return (strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub)
 		return (NULL);
-	my_strlcpy(sub, s + start, max_len + 1);
+	my_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
