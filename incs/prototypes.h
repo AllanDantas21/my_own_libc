4d7d69ec7ff7b7a8eba6ab802361164962ae38a5
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-03 21:09:42 -0300

    add some btree funcs

--- incs/prototypes.h
+++ incs/prototypes.h
@@ -51,10 +51,10 @@ void	ft_rev_int_tab(int *tab, int size);
 t_list	*my_lstnew(void *content);
 t_list	*my_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*my_lstlast(t_list *lst);
-t_list *my_list_at(t_list *begin_list, unsigned int nbr);
+t_list  *my_list_at(t_list *begin_list, unsigned int nbr);
 t_list  *my_list_find(t_list *begin_list, void *data_ref, int (*cmp)());
-void my_list_foreach_if(t_list *begin_list, void (*f)(void *), void *data_ref, int (*cmp)());
-void my_list_foreach(t_list *begin_list, void (*f)(void *));
+void    my_list_foreach_if(t_list *begin_list, void (*f)(void *), void *data_ref, int (*cmp)());
+void    my_list_foreach(t_list *begin_list, void (*f)(void *));
 void	my_lstiter(t_list *lst, void (*f)(void *));
 void	my_lstdelone(t_list *lst, void (*del)(void *));
 void	my_lstclear(t_list **lst, void (*del)(void *));
@@ -65,6 +65,11 @@ void    my_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)());
 void    my_list_merge(t_list **begin_list1, t_list *begin_list2);
 void    my_list_sort(t_list **begin_list, int (*cmp)());
 void    my_sorted_list_merge(t_list **begin_list1, t_list *begin_list2, int (*cmp)());
+
+// btree functions
+void btree_apply_suffix(t_btree *root, void (*applyf)(void *));
+void btree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void btree_apply_infix(t_btree *root, void (*applyf)(void *));
 
 // printf functions
 int     my_printf_address(void *pointer);
---
+++ srcs/btree/btree_apply_prefix.c
@@ -0,0 +1,10 @@
+#include "my_libc.h"
+
+void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+    if (!root)
+        return ;
+    applyf(root->item);
+    btree_apply_prefix(root->left, applyf);
+    btree_apply_prefix(root->right, applyf);
+}
