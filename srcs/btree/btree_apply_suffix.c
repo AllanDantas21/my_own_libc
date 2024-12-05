3ab29f9270cf3149eef980fd67f402dc91e39451
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-05 10:36:47 -0300

    add docs in btree

--- srcs/btree/btree_apply_suffix.c
+++ srcs/btree/btree_apply_suffix.c
@@ -1,5 +1,15 @@
 #include "my_libc.h"
 
+/**
+ * btree_apply_suffix - Applies a function to each node of a binary tree using
+ *                      suffix (post-order) traversal.
+ * @root: Pointer to the root node of the binary tree.
+ * @applyf: Function to apply to each node's item.
+ *
+ * This function traverses the binary tree in post-order (left, right, root)
+ * and applies the given function to each node's item.
+ */
+ 
 void btree_apply_suffix(t_btree *root, void (*applyf)(void *))
 {
     if (!root)
--- srcs/btree/btree_create_node.c
+++ srcs/btree/btree_create_node.c
@@ -1,5 +1,22 @@
 #include "my_libc.h"
 
+/**
+ * @file btree_create_node.c
+ * @brief This file contains the function to create a new binary tree node.
+ */
+
+/**
+ * @brief Creates a new binary tree node.
+ *
+ * This function allocates memory for a new binary tree node and initializes
+ * its item with the provided value. The left and right children of the node
+ * are set to NULL.
+ *
+ * @param item A pointer to the item to be stored in the new node.
+ * @return A pointer to the newly created binary tree node, or NULL if memory
+ * allocation fails.
+ */
+ 
 t_btree *btree_create_node(void *item)
 {
     t_btree *node;
