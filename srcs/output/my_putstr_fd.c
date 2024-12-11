3f4ad795e5f82aa6003b7b8426412689cbf49f1e
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-10 21:32:57 -0300

    add docs

--- srcs/output/my_putstr_fd.c
+++ srcs/output/my_putstr_fd.c
@@ -1,4 +1,14 @@
 #include "my_libc.h"
+/**
+ * @file my_putstr_fd.c
+ * @brief Outputs a string to the given file descriptor.
+ *
+ * This function writes the string `s` to the file descriptor `fd`.
+ * If the string `s` is NULL, the function does nothing.
+ *
+ * @param s The string to be written.
+ * @param fd The file descriptor to write to.
+ */
 
 void	my_putstr_fd(char *s, int fd)
 {
