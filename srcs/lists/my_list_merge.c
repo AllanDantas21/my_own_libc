e1a70f75fd26d0607fb509d7ab97c708766f6970
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-14 23:02:18 -0300

    att

--- srcs/lists/my_list_merge.c
+++ srcs/lists/my_list_merge.c
@@ -1,5 +1,18 @@
 #include "my_libc.h"
 
+/**
+ * @file my_list_merge.c
+ * @brief Merges two linked lists.
+ *
+ * This function takes two linked lists and merges the second list into the first one.
+ *
+ * @param begin_list1 A pointer to the pointer of the first linked list.
+ * @param begin_list2 A pointer to the second linked list.
+ *
+ * @note The function assumes that both lists are non-empty and that the first list
+ *       has at least one element.
+ */
+ 
 void my_list_merge(t_list **begin_list1, t_list *begin_list2)
 {
     t_list *tmp;
